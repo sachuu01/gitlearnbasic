72fe875f473c66193e7422a5f7e0a5320458c16d
sachinjadhav <sajadhav51@yahoo.com>
2019-11-16 18:23:50 +0530

    sajadhav commited on local

--- MavenDemo/src/test/java/MavenProj/MavenDemo/Calculation.java
+++ MavenDemo/src/test/java/MavenProj/MavenDemo/Calculation.java
@@ -1,9 +1,25 @@
 package MavenProj.MavenDemo;
 
 public class Calculation {
+	int a;
+	int b;
+	int c;
+	
+	
+	public void calculator()
+	{
+		a=10;
+		b=2;
+		c= a*b;
+		System.out.println("Multipication of Two No : " +c);
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Calculation cal= new Calculation();
+		
+		cal.calculator();
 
 	}
 
--- MavenDemo/src/test/java/MavenProj/MavenDemo/Testone.java
+++ MavenDemo/src/test/java/MavenProj/MavenDemo/Testone.java
@@ -11,8 +11,7 @@ public class Testone {
 	{
 		 a= 10;
 		 b= 20;
-		
-	     c= a+b;
+		 c= a+b;
 	     
 	     System.out.println("Addtion of a and b = " +c);
 	
